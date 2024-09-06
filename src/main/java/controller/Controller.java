ac06c00992fc9be095683713742f452dc69371bc
Cristian Guedes <cristian.edel@yahoo.com>
2024-09-05 21:04:31 -0300

    JAVA WEB

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,28 +6,48 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import model.DAO;
+import model.JavaBeans;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	DAO dao = new DAO();
+	JavaBeans javaBeans = new JavaBeans();
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Controller() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		if (action.equals("/main")) {
+			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
+		}
+	}
+
+	// Listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
+
+	// Novo contatos
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//teste de rescebimento dos dados do formulário
+//		System.out.println(request.getParameter("nome"));
+//		System.out.println(request.getParameter("fone"));
+//		System.out.println(request.getParameter("email"));
+		//setar as variaveis JavaBeans
+		javaBeans.setNome(request.getParameter("nome"));
+		javaBeans.setFone(request.getParameter("fone"));
+		javaBeans.setEmail(request.getParameter("email"));
 	}
 
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,37 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/** Modulo de conexao **/
+	// Parametros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/agenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String passwords = "Cc01102003+";
+
+	// Métodos de conexao
+	private Connection conectar() {
+		Connection con;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, passwords);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	// teste de conexao
+	public void testeconexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
